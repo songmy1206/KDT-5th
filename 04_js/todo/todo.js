4ffa46d102e91b0b7f73bb2c433dd1e926cac266
songminyoung <songmy1206@gmail.com>
2023-02-15 09:11:22 +0900

    todolist 수정

--- 04_js/todo/todo.js
+++ 04_js/todo/todo.js
@@ -1,14 +1,36 @@
 const addBtn = document.querySelector(".input-btn");
 const todoList = document.querySelector(".todo-list");
 const inputTask = document.querySelector(".input-task");
-
-const btn = document.createElement("button");
-btn.textContent = "추가";
-
-const checkBox = document.createElement("input");
-checkBox.setAttribute("type", "checkbox");
   
 addBtn.addEventListener("click", function(){
-  todoList.append(btn);
-  todoList.prepend(checkBox);
+  const checkBoxEl = document.createElement("input");
+  checkBoxEl.setAttribute("type", "checkbox");
+  const liEl = document.createElement("li");
+  liEl.textContent = inputTask.value;
+  const btnEl = document.createElement("button");
+  btnEl.textContent = "삭제";
+
+  if(liEl.textContent === ""){
+    inputTask.setAttribute("placeholder", "내용을 입력하세요!");
+  }else{
+    todoList.append(liEl);
+    liEl.append(btnEl);
+    liEl.prepend(checkBoxEl);
+  }
+
+  checkBoxEl.addEventListener("click", function(){
+    if(checkBoxEl.checked === true){
+      liEl.setAttribute("style", "text-decoration: line-through");
+    }else{
+      liEl.setAttribute("style", "text-decoration: none");
+    }
+  })
+
+  btnEl.addEventListener("click", function(){
+    if(checkBoxEl.checked === true){
+      liEl.remove();
+    }else{
+      alert("삭제할 항목을 선택해주세요");
+    }
+  })
 })
